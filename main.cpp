862bb5af839a00665d9d85106df358b25c1539c8
DDHunter13 <1310nero@mail.ru>
2015-03-21 13:15:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
-#include "Labyrinth.h"
+#include "Player.h"
 
 int main(void) {
+	srand(time(0));
+	int width = 12;
+	int height = 11;
+	int finishI = 1 + rand () % (height - 1);
+	int finishJ = 1 + rand () % (width - 1);
+	int startI = 1 + rand() % (height - 1);
+	int startJ = 1 + rand() % (width - 1);
 
-	int width = 6;
-	int height = 6;
-	Lab Map(width, height);
+	Lab Map(width, height, finishI, finishJ);
 
-	for (int i = 1; i < height; ++i) { 
-		Map.Arr[i][0].right_wall = 1;
-		Map.Arr[i][width - 1].right_wall = 1;
-	}
-	for (int i = 1; i < width; ++i) { 
-		Map.Arr[0][i].down_wall = 1; 
-		Map.Arr[height - 1][i].down_wall = 1;
-	}
-
-	Map.Arr[1][1].right_wall = Map.Arr[1][3].right_wall = Map.Arr[2][2].right_wall = Map.Arr[2][3].right_wall = Map.Arr[2][4].right_wall =
-		Map.Arr[3][1].right_wall = Map.Arr[3][3].right_wall = Map.Arr[3][4].right_wall = Map.Arr[4][2].right_wall = Map.Arr[4][4].right_wall =
-		Map.Arr[5][3].right_wall = 1;
-
-	Map.Arr[1][2].down_wall = Map.Arr[2][1].down_wall = Map.Arr[2][5].down_wall = Map.Arr[4][1].down_wall = Map.Arr[4][3].down_wall = 1;
-
+	Map.CreateLab();
+	Player PL(Map, startI, startJ);
 	std::cout << Map;
 
+
+
+	PL.Move(Map, finishI, finishJ);
+
+	system("pause");
 	return 0;
 }
