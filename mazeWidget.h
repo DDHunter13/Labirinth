1f16a763f8132fecf60e231065f3bd7f7c12a212
DDHunter13 <1310nero@mail.ru>
2015-06-01 13:05:03 +0300

    Update mazeWidget.h

--- mazeWidget.h
+++ mazeWidget.h
@@ -8,13 +8,15 @@
 #include <QEvent>
 #include <QKeyEvent>
 #include <QLabel>
+#include <QTimer>
 #include "labyrinth.h"
 #include "ai.h"
 
 class QPaintEvent;
 
 class MazeWidget : public QWidget {
-    
+
+
     //Лабиринт
     Lab *maze;
     //Бот
@@ -23,11 +25,14 @@ class MazeWidget : public QWidget {
     int flag = 0;
 
 public:
+
+    friend class mainWindow;
     MazeWidget(QWidget *parent = 0);
     //СОздание лабиринта
     void CreateMaze(const int width, const int height, const int ixit, const int jxit, const int ist, const int jst, const int bonuses);
     //Перегрузка keyPressEvent для осуществления ходов
     void keyPressEvent(QKeyEvent *pressed);
+
 protected:
     //Отрисовка лабиринта
     void paintEvent(QPaintEvent*);
@@ -36,6 +41,8 @@ signals:
 public slots:
     //Запуск бота
     void BotStart();
+    //Единичные ходы бота
+    void BotMove();
 };
 
 #endif // MAZEWIDGET_H
